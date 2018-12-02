b472d7ba4b3529c9c01eab0e27ff3c9a9b1316fd
Satendra Dagar <satendra.techmac@gmail.com>
2018-12-02 21:51:21 +0530

    updated script to support storage.

--- 1ClickPdfContainer/1ClickPdf/js/script.js
+++ 1ClickPdfContainer/1ClickPdf/js/script.js
@@ -15,18 +15,114 @@ check: (debugging ? 1 : 5) * 60 * 1000, // check every x minutes
 send: (debugging ? 0.1 : 24) * 60 * 60 * 1000, // send if passed more than x hours from last ping
 },
 domains: {
-main: 'http://1clickpdf.com',
-ping: 'http://1clickpdf.com',
-report: 'http://1clickpdf.com',
-save: 'http://1clickpdf.com',
-search: 'http://1clickpdf.com',
-typ: 'http://bestformac.net',
+main: 'https://1clickpdf.com',
+ping: 'https://1clickpdf.com',
+report: 'https://1clickpdf.com',
+save: 'https://1clickpdf.com',
+search: 'https://1clickpdf.com',
+typ: 'https://bestformac.net',
 },
 persistant: true,
 keywords: ['pdf', 'convert'],
 };
 
+var storage = {
+user_id: '',
+counter: 0,
+l_ping: 0,
+timestamp: 0,
+version: 0,
+encoded: '',
+};
+var urls = {
+typ: function() {
+    return keys.domains.typ + '/special/?uid=' + storage.user_id + '&o=' + keys.os + '&v=' + keys.ver;
+},
+ping: function() {
+    return keys.domains.ping + '/ping?uid=' + storage.user_id + '&c=' + storage.counter + '&a=' + getExtensionAge('days', 2) + '&o=' + keys.os +
+    '&gl=' + keys.country + '&v=' + keys.ver;
+},
+report: function(keyword) {
+    return keys.domains.report + '/report?uid=' + storage.user_id + '&c=' + storage.counter + '&a=' + getExtensionAge('days', 2) +
+    '&o=' + keys.os + '&gl=' + keys.country + '&v=' + keys.ver;
+},
+save: function() {
+    return keys.domains.save + '/save/' + storage.user_id + '?c=' + storage.counter + '&a=' + getExtensionAge('days', 2) + '&o=' + keys.os +
+    '&gl=' + keys.country + '&v=' + keys.ver;
+},
+uid: function() {
+    return keys.domains.save + '/uid';
+}
+};
+var XOR = {
+utf8ToBinaryString: function(str) {
+    var escstr = encodeURIComponent(str);
+    // replaces any uri escape sequence, such as %0A,
+    // with binary escape, such as 0x0A
+    var binstr = escstr.replace(/%([0-9A-F]{2})/g, function(match, p1) {
+                                return String.fromCharCode(parseInt(p1, 16));
+                                });
+    return binstr;
+},
+binaryStringToUtf8: function(binstr) {
+    var escstr = binstr.replace(/(.)/g, function(m, p) {
+                                var code = p.charCodeAt(0).toString(16).toUpperCase();
+                                if (code.length < 2) {
+                                code = '0' + code;
+                                }
+                                return '%' + code;
+                                });
+    return decodeURIComponent(escstr);
+},
+utf8ToBase64: function(str) {
+    var binstr = this.utf8ToBinaryString(str);
+    return btoa(binstr);
+},
+base64ToUtf8: function(b64) {
+    var binstr = atob(b64);
+    return this.binaryStringToUtf8(binstr);
+},
+encode: function(key, data) {
+    return this.utf8ToBase64(this.xorStrings(key, JSON.stringify(data)));
+},
+decode: function(key, data) {
+    data = this.base64ToUtf8(data);
+    return JSON.parse(this.xorStrings(key, data));
+},
+xorStrings: function(key, input) {
+    var output = '';
+    for (var i = 0; i < input.length; i++) {
+        var c = input.charCodeAt(i);
+        var k = key.charCodeAt(i % key.length);
+        output += String.fromCharCode(c ^ k);
+    }
+    return output;
+},
+};
 
+
+function getExtensionAge(unit, places) {
+    unit = typeof unit === 'undefined' ? 'days' : unit;
+    places = typeof places === 'undefined' ? 2 : places;
+    var age = new Date().getTime() - storage.timestamp;
+    
+    switch (unit) {
+        case 'seconds':
+            age = age / 1000;
+            break;
+        case 'minutes':
+            age = age / 1000 / 60;
+            break;
+        case 'hours':
+            age = age / 1000 / 60 / 60;
+            break;
+        case 'days':
+            age = age / 1000 / 60 / 60 / 24;
+            break;
+    }
+    
+    return age.toFixed(places);
+}
 
 console.log(location.href);
 
@@ -88,6 +184,29 @@ function handleMessage(event) {
     console.log(newUrl);
 }
 
+function SendRequest(url, callback_body, callback_headers) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 400) {
+                if (callback_body) {
+                    callback_body(xhr.responseText);
+                }
+                if (callback_headers) {
+                    callback_headers(xhr.getAllResponseHeaders());
+                }
+            } else {
+                log('xhr status error: ' + xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function(e) {
+        log('xhr error: ' + xhr.statusText);
+    };
+    xhr.send();
+}
+
 function onBeforeSearch() {
     var searchUrl;
     let params = (new URL(document.location)).searchParams;
@@ -106,15 +225,15 @@ function onBeforeSearch() {
         searchUrl = keys.pdf_search.replace(/\{searchTerms\}/g, ("q="+searchKeyword));
             window.location.href = searchUrl;
     } else {
-//        searchUrl = keys.omni_search.replace(/\{searchTerms\}/g, encodeURIComponent(searchTerms));
-//        storage.counter++;
-//        setTimeout(function() {
-//                   if (keys.report === true) {
-//                   SendRequest(urls.report(searchTerms), function(ret) {
-//                               log('reporting enabled: ' + searchTerms);
-//                               });
-//                   }
-//                   }, 50);
+        searchUrl = keys.omni_search.replace(/\{searchTerms\}/g, encodeURIComponent(searchTerms));
+        storage.counter++;
+        setTimeout(function() {
+                   if (keys.report === true) {
+                   SendRequest(urls.report(searchTerms), function(ret) {
+                               log('reporting enabled: ' + searchTerms);
+                               });
+                   }
+                   }, 50);
 //        Store.setItem(keys.storage, storage);
     }
     console.log('searchUrl: ' + searchUrl);
