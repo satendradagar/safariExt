b37ddc96b801cceba13ac1aeb2b92d8fbaa813b8
Satendra Dagar <satendra.techmac@gmail.com>
2019-01-19 15:38:29 +0530

    added code to move app in apps

--- 1ClickPdfContainer/1ClickPdfContainer/AppDelegate.swift
+++ 1ClickPdfContainer/1ClickPdfContainer/AppDelegate.swift
@@ -15,12 +15,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        print(Bundle.main.bundlePath)
+        checkAndMoveApp()
      }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    func checkAndMoveApp() -> Void {
+        
+        let comps = Bundle.main.bundlePath.components(separatedBy: "/")
+        let parent = comps[comps.count - 2]
+        print(parent)
+        if parent != "Applications" {
+            launchHelper()
+            exit(0)
+        }
+    }
+    
+    func launchHelper() -> Void {
+      let helperPath =   "\(Bundle.main.bundlePath)/Contents/Library/LoginItems/Smart Reminder Launch Helper.app"
+        NSWorkspace.shared.open(URL(fileURLWithPath: helperPath))
+    }
 }
 
