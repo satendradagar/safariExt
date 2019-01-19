25d64d8608730aef8c2da0ac118969df7ffabef4
Satendra Dagar <satendra.techmac@gmail.com>
2019-01-19 15:39:12 +0530

    added method to control app window

---
+++ 1ClickPdfContainer/1ClickPdfContainer/frontmost_window.swift
@@ -0,0 +1,16 @@
+import Cocoa
+
+//let workspace = NSWorkspace.shared
+//let activeApps = workspace.runningApplications
+//for app in activeApps {
+//    if app.isActive {
+//        let listOptions = CGWindowListOption(arrayLiteral: CGWindowListOption.optionOnScreenOnly)
+//        let windowList : NSArray = CGWindowListCopyWindowInfo(listOptions, 0)!
+//        for window in windowList {
+//            if  (window["kCGWindowOwnerName"] as! String) == app.localizedName {
+//                let topWindow = window as! NSDictionary
+//                break
+//            }
+//        }
+//    }
+//}
